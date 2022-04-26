ac5bd8a43c250cc431d2c1af15e9f8086399806e
Irina <parakerta@mail.ru>
2022-04-26 16:40:48 +0300

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,41 @@
 <template>
-<footer class="footer">
-
-</footer>
+  <footer class="footer p-5 mt-3 sticky-bottom">
+    <div class="row">
+      <div class="col-md">
+        <ul>
+          <li>
+            <NuxtLink to="/">Способы оплаты</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/"> Доставка</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Возврат товара</NuxtLink>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md">
+        <ul>
+          <li>
+            <NuxtLink to="/">О нас</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Реквизиты</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Контакты</NuxtLink>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md text-center">
+        <p>
+          2022 Ярмакофф - модный интернет-магазин одежды от российских
+          производителей.<br />
+          Все права защищены. Доставка по всей России
+        </p>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -10,8 +44,26 @@ export default {
 }
 </script>
 <style scoped>
-.footer{
-    background-color: #B56A0E;
-    color: #fff;
+.footer {
+  background-color: #b56a0e;
+  color: #fff;
+}
+.footer a {
+  color: #fff;
+  text-decoration: none;
+}
+ul li {
+  list-style-type: none;
+}
+@media screen and (max-width:768px) {
+    .col-md{
+        text-align: center;
+    }
+    ul{
+        margin: 0;
+    }
+    p{
+        margin-top: 20px;
+    }
 }
 </style>
