1b4cc946c854cca8aca9491954aec66cec7f4e3b
Irina <parakerta@mail.ru>
2022-04-27 13:05:03 +0300

    card fix

--- components/CategoryCatalog.vue
+++ components/CategoryCatalog.vue
@@ -2,13 +2,13 @@
   <div class="container mt-5">
     
     <div class="row">
-      <div class="col-3">
+      <div class="col-sm-3">
           <div class="sticky-nav">
    <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="#">Каталог</a></li>
       <li class="breadcrumb-item active" aria-current="page">Платья</li>
     </ol>
-        <ul class="nav flex-column">
+        <ul class="nav flex-md-column">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Все модели</a>
           </li>
@@ -25,7 +25,7 @@
           </div>
        
       </div>
-      <div class="col-9">
+      <div class="col-sm-9">
           <h2>Все модели</h2>
           <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
               <ItemCard/>
--- components/ItemCard.vue
+++ components/ItemCard.vue
@@ -3,8 +3,15 @@
     <div class="card">
         <img src="~/assets/item_card.png" class="card-img-top" />
       <div class="card-body">
-        <h5 class="card-title">Название карточки</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">Платье "Яркая мадама"</h5>
+        <p class="card-text text-danger">Бренд Тутушка</p>
+        <div class="clearfix">
+            <div class="float-end">
+ <p class="card-text">4566 РУБ</p>
+            </div>
+
+        </div>
+       
       </div>
     </div>
   </div>
@@ -19,5 +26,10 @@ img{
     width: 100%;
     object-fit: cover;
     pointer-events: none;
+}
+.card{
+    border: none;
+    border-radius: 0;
+    border-bottom: 2px solid #000;
 }
 </style>
