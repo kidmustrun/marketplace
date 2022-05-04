0b87f7821b1a200132e9da8b20b1e974d609346e
Irina <parakerta@mail.ru>
2022-05-04 22:10:45 +0300

    cart

---
+++ components/CartItem.vue
@@ -0,0 +1,81 @@
+<template>
+  <tr>
+    <td>
+      <img src="~/assets/dress_in_block.png" class="img-fluid rounded-start" />
+    </td>
+    <td>
+      Женское платье<br /><b>Звезда взошла</b><br /><br />Размер 48, M<br />Артикул:
+      09ОАМС5
+    </td>
+    <td>2050 РУБ</td>
+    <td>
+      <div class="input-group w-50 mx-auto">
+        <input
+          type="button"
+          value="-"
+          id="button_minus"
+          class="counting p-3"
+          @click="reduceOne()"
+        />
+        <input
+          type="number"
+          id="num_count"
+          name="quantity"
+          v-model="count"
+          class="text-center form-control"
+        />
+
+        <input
+          type="button"
+          value="+"
+          id="button_plus"
+          class="counting p-3"
+          @click="addOne()"
+        />
+      </div>
+    </td>
+    <td>{{full_cost}} РУБ</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'CartItem',
+  data() {
+    return {
+      count: 1,
+      cost: 2050,
+      full_cost: 2050
+    }
+  },
+  methods: {
+    reduceOne() {
+      if (this.count >= 2) {
+        this.count--
+        this.full_cost = this.cost * this.count
+      }
+    },
+    addOne() {
+      this.count++
+      this.full_cost = this.cost * this.count
+    },
+  },
+}
+</script>
+<style scoped>
+img {
+  height: 30vh;
+
+  object-fit: cover;
+  object-position: center;
+}
+.counting {
+  border: none;
+  background-color: transparent;
+}
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0; 
+}
+</style>
---
+++ pages/cart/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="cart">
+    <HeaderBackground />
+    <div class="container mt-5">
+      <h1>Корзина</h1>
+      <div class="table-responsive">
+        <table class="table align-middle text-center">
+          <thead>
+            <tr>
+              <th class="col">Фотография</th>
+              <th class="col">Товар</th>
+              <th class="col">Цена</th>
+              <th class="col">Количество</th>
+              <th class="col">Стоимость</th>
+            </tr>
+          </thead>
+          <tbody>
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+          </tbody>
+        </table>
+      </div>
+      <h3 class="float-end">Итого: 7050 РУБ</h3>
+      <br>
+      <br>
+      <button class="button_create px-md-5 py-md-2 p-2 float-end">Оформить заказ</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+<style scoped>
+.button_create{
+    background: #0D2C48;
+border-radius: 10px;
+color: #fff;
+border: none;
+transition: 0.3s ease;
+text-decoration: none;
+}
+.button_create:hover{
+    background: #144169;
+    transition: 0.3s ease;
+}
+</style>
